9248b49d5d1c0cf98cd976f0f79bf59e88cccb76
Mr.Mikey <1303232158@qq.com>
2022-05-23 20:49:48 +0800

    docs: lint-staged.config.js
    添加说明注释

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -4,5 +4,6 @@
  * 配置的命令可以填多个，使用数组即可
  */
 module.exports = {
+  // 对匹配的文件执行prettier格式化、eslint修复，再进行eslint检测，如果还有不通过的则需要自己手动修复
   '*.{js,jsx,vue,ts,tsx}': ['prettier --ignore-unknown --write', 'eslint --fix', 'npm run lint']
 }
