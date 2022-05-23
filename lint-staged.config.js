7b69e0550fb3a0264fd14d88b84dbadcf733d06f
Mr.Mikey <1303232158@qq.com>
2022-05-24 00:46:01 +0800

    chore: 添加lint-staged配置文件说明

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,9 @@
 /**
  * 使用git commit提交代码时会触发git hooks(pre-commit)
  *
+ * 之所以能在package.json中配置githooks生效，是因为使用vue-create创建的项目会安装
+ * 尤大fork自husky的yorkie包，使用起来更方便
+ *
  * 配置的命令可以填多个，使用数组即可
  */
 module.exports = {
