bcb0e2c18b2297d3410403659fa1cdf714c4dbb1
Mr.Mikey <1303232158@qq.com>
2022-05-23 20:37:07 +0800

    chore: test

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,8 @@
 /**
  * 使用git commit提交代码时会触发git hooks(pre-commit)
  *
- * 配置的命令可以填多个，规则与package.json中的scripts保持一致
+ * 配置的命令可以填多个，使用数组即可
  */
 module.exports = {
-  '*.{js,jsx,vue,ts,tsx}': ['npm run lint', 'npm run release']
+  '*.{js,jsx,vue,ts,tsx}': 'npm run lint'
 }
