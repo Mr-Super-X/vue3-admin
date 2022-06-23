479433cf1cf7acee8a63620076e278dc02780363
v_jpch <v_jpch@digitalgd.com.cn>
2022-06-23 17:36:16 +0800

    [*]: t

--- verifyCommit.js
+++ verifyCommit.js
@@ -2,16 +2,19 @@ const msg = require('fs')
   .readFileSync('.git/COMMIT_EDITMSG', 'utf-8')
   .trim()
 
-const commitRE = /^(revert: )?('[+]'|'[!]'|'[*]'|'[-]')(\(.+\))?: .{1,50}/
+const commitRE = /^(revert: )?(\[\+\]|\[!\]|\[\*\]|\[-\])(\(.+\))?: .{1,50}/
 const mergeRe = /^(Merge pull request|Merge branch)/
 if (!commitRE.test(msg)) {
   if (!mergeRe.test(msg)) {
     console.log('git commit信息校验不通过')
 
-    console.error(`git commit的信息格式不对, 需要使用 title(scope): desc的格式
-      比如 [!]: 修复Bug
-      [+](test): 添加新功能
-      具体校验逻辑看 verifyCommit.js
+    console.error(`git commit的信息格式不对, 需要使用 type: desc的格式
+      例如：
+
+      [!]: Bug修改
+      [+]: 新增功能
+      [*]: 常规修改
+      [-]: 删除功能
     `)
     process.exit(1)
   }
