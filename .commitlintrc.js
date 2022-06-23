8fbadac18adb1f9ba577ed6330345ee7353ff8f1
v_jpch <v_jpch@digitalgd.com.cn>
2022-06-23 14:15:15 +0800

    [*](other): add rules

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -4,7 +4,8 @@ module.exports = {
     'type-enum': [
       2,
       'always',
-      ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test']
+      // ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test']
+      ['[!]', '[+]', '[*]', '[-]']
     ]
   }
 }
--- .cz-config.js
+++ .cz-config.js
@@ -4,33 +4,37 @@
 module.exports = {
   // type 类型（定义之后，可通过上下键选择）
   types: [
-    { value: 'feat', name: 'feat: 新增功能' },
-    { value: 'fix', name: 'fix: 修复 bug' },
-    { value: 'docs', name: 'docs: 文档变更' },
-    {
-      value: 'chore',
-      name: 'chore: 对构建过程或辅助工具和库的更改（不影响源文件、测试用例）'
-    },
-    {
-      value: 'style',
-      name: 'style: 代码格式（不影响功能，例如空格、分号等格式修正）'
-    },
-    {
-      value: 'refactor',
-      name: 'refactor: 代码重构（不包括 bug 修复、功能新增）'
-    },
-    { value: 'perf', name: 'perf: 性能优化' },
-    { value: 'test', name: 'test: 添加、修改测试用例' },
-    {
-      value: 'build',
-      name: 'build: 构建流程、外部依赖变更（如升级 npm 包、修改 webpack 配置等）'
-    },
-    { value: 'ci', name: 'ci: 修改 CI 配置、脚本' },
-    {
-      value: 'test',
-      name: 'test: 添加缺失的测试或纠正现有的测试'
-    },
-    { value: 'revert', name: 'revert: 回滚 commit' }
+    { value: '[!]', name: '[!]: Bug修改' },
+    { value: '[+]', name: '[+]: 新增功能' },
+    { value: '[*]', name: '[*]: 常规修改' },
+    { value: '[-]', name: '[-]: 删除功能' },
+
+    // { value: 'fix', name: 'fix: 修复 bug' },
+    // { value: 'docs', name: 'docs: 文档变更' },
+    // {
+    //   value: 'chore',
+    //   name: 'chore: 对构建过程或辅助工具和库的更改（不影响源文件、测试用例）'
+    // },
+    // {
+    //   value: 'style',
+    //   name: 'style: 代码格式（不影响功能，例如空格、分号等格式修正）'
+    // },
+    // {
+    //   value: 'refactor',
+    //   name: 'refactor: 代码重构（不包括 bug 修复、功能新增）'
+    // },
+    // { value: 'perf', name: 'perf: 性能优化' },
+    // { value: 'test', name: 'test: 添加、修改测试用例' },
+    // {
+    //   value: 'build',
+    //   name: 'build: 构建流程、外部依赖变更（如升级 npm 包、修改 webpack 配置等）'
+    // },
+    // { value: 'ci', name: 'ci: 修改 CI 配置、脚本' },
+    // {
+    //   value: 'test',
+    //   name: 'test: 添加缺失的测试或纠正现有的测试'
+    // },
+    // { value: 'revert', name: 'revert: 回滚 commit' }
   ],
   // scope 类型（定义之后，可通过上下键选择）
   scopes: [
