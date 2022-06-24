cc8ccd9aaff1e7a08f7ef6192f98f7dec50b545a
v_jpch <v_jpch@digitalgd.com.cn>
2022-06-24 19:49:45 +0800

    tt(1): t

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  // 继承cz配置，依赖commitlint-config-cz包（实测这两个都要写，否则不生效）
-  extends: ['@commitlint/config-conventional'],
+  // 继承cz-config配置，依赖commitlint-config-cz包（实测这两个都要写，否则不生效）
+  extends: ['@commitlint/config-conventional', 'cz'],
 }
