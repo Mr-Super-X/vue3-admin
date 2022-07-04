58cb908e394aee9131e76e43c94c3c961cdd7e6b
v_jpch <v_jpch@digitalgd.com.cn>
2022-07-04 22:00:46 +0800

    chore(other): add types dir

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -18,7 +18,7 @@ interface IListItem {
 
 // <IListItem[]>表示list为数组类型，所有的子项需要符合IListItem接口定义的格式
 // 好处是模板中使用list和item时，鼠标移入会有类型提示
-const list = ref([
+const list = ref<IListItem[]>([
   {
     id: '1',
     text: 'test1',
@@ -26,10 +26,6 @@ const list = ref([
   {
     id: '2',
     text: 'test2',
-  },
-  {
-    id: '3',
-    text: 'test3',
   },
 ])
 </script>
