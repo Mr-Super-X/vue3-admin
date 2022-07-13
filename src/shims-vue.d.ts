067e13ab22714fd39be0421f3e8522da4cdea014
Mr.Mikey <v_jpch@digitalgd.com.cn>
2022-07-14 03:40:19 +0800

    feat: 添加nprogress组件

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -7,7 +7,11 @@ declare module '*.vue' {
 
 // 修复找不到mockjs模块的ts报错
 declare module 'mockjs'
+
 // 修复找不到vue模块的ts报错
 // 如果遇到找不到vue模块的报错，不要在这里定义vue模块，会导致在script setup ts语法中定义的变量
 // 在template中找不到，提示报错any 类型“{}”上不存在属性“xxx”
 // declare module 'vue'
+
+// 修复找不到nprogress模块的ts报错
+declare module 'nprogress'
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,3 @@
 export * from './common' // 导出common模块所有的配置
+export * from './validate' // 导出validate模块所有的配置
 // export * from './configs' // 导出configs模块所有配置
