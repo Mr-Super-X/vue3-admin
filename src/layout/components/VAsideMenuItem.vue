7bab620f06cdb8441302113f7a6d51a5209f1f09
Mr.Mikey <v_jpch@digitalgd.com.cn>
2023-06-20 11:41:54 +0800

    chore(components): 优化菜单组件

--- src/layout/components/VAsideMenuItem.vue
+++ src/layout/components/VAsideMenuItem.vue
@@ -1,30 +1,28 @@
-<template>
-  <!-- meta.isHide为true的数据不显示 -->
-  <!-- 破案了，这里不能用其他标签代替，否则会导致el-menu collapse属性为true时不能正确隐藏内容 -->
-  <template v-if="!item.meta.isHide">
-    <el-sub-menu v-if="item?.children?.length" :index="item.path">
-      <template #title>
-        <!-- 
+<!-- meta.isHide为true的数据不显示 -->
+<!-- 这里不能嵌套其他标签，否则会导致el-menu collapse属性为true时不能正确隐藏内容 -->
+<template v-if="!item.meta.isHide">
+  <el-sub-menu v-if="item?.children?.length" :index="item.path">
+    <template #title>
+      <!-- 
           出现runtime-core.esm-bundler.js [Vue warn]: Invalid vnode type when creating vnode: . 
-          时，表示当前item.meta.icon没有设置值
+          时，表示当前item.meta.icon没有设置值，因此需要v-if屏蔽掉该错误
         -->
-        <v-element-icon :name="item.meta.icon" />
-        <span>{{ item.meta.title }}</span>
-      </template>
-      <v-aside-menu-item v-for="child in item.children" :key="child.id" :item="child" />
-    </el-sub-menu>
+      <v-element-icon v-if="item.meta.icon" :name="item.meta.icon" />
+      <span>{{ item.meta.title }}</span>
+    </template>
+    <v-aside-menu-item v-for="child in item.children" :key="child.path" :item="child" />
+  </el-sub-menu>
 
-    <el-menu-item v-else :index="item.path">
-      <template #title>
-        <!-- 
+  <el-menu-item v-else :index="item.path" :key="item.path">
+    <!-- 
           出现runtime-core.esm-bundler.js [Vue warn]: Invalid vnode type when creating vnode: . 
-          时，表示当前item.meta.icon没有设置值
+          时，表示当前item.meta.icon没有设置值，因此需要v-if屏蔽掉该错误
         -->
-        <v-element-icon :name="item.meta.icon" />
-        <span>{{ item.meta.title }}</span>
-      </template>
-    </el-menu-item>
-  </template>
+    <v-element-icon v-if="item.meta.icon" :name="item.meta.icon" />
+    <template #title>
+      <span>{{ item.meta.title }}</span>
+    </template>
+  </el-menu-item>
 </template>
 
 <script setup lang="ts">
--- src/views/modules/order/orderInfo/routes.ts
+++ src/views/modules/order/orderInfo/routes.ts
@@ -11,7 +11,7 @@ export default [
       isHide: false,
       isAffix: false,
       isIframe: false,
-      icon: 'i-ep-ChatDotSquare',
+      icon: '',
     },
   },
 ]
