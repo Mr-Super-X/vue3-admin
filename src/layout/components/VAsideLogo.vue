a79d590e998c998d3caf15fb6fa58dcabeb25e2a
Mr.Mikey <v_jpch@digitalgd.com.cn>
2023-06-28 09:24:35 +0800

    feat(assets): 添加svg格式logo

--- src/layout/components/VAsideLogo.vue
+++ src/layout/components/VAsideLogo.vue
@@ -5,21 +5,25 @@
  * @Contact: 1303232158@qq.com
  * @Date: 2022-05-31 11:59:06
  * @LastEditors: Mr.Mikey
- * @LastEditTime: 2023-06-27 18:04:08
+ * @LastEditTime: 2023-06-28 09:21:48
  * @FilePath: \vue3-admin\src\layout\components\VAsideLogo.vue
 -->
 
 <template>
   <div class="layout-logo">
-    <img src="../../assets/images/logo.png" alt="vue3-admin-logo" @click="handleGoHome" />
-    <h1 class="title">vue3-admin</h1>
+    <img src="../../assets/images/logo.svg" alt="vue3-admin-logo" @click="handleGoHome" />
+    <!-- <h1 class="title">{{ themeConfig.globalTitle }}</h1> -->
   </div>
 </template>
 
 <script lang="ts" setup>
+import { storeToRefs } from 'pinia'
 import { useRouter } from 'vue-router'
+import { useThemeConfigStore } from '@store/modules/themeConfig'
 
 const router = useRouter()
+const themeConfigStore = useThemeConfigStore()
+const { themeConfig } = storeToRefs(themeConfigStore)
 
 const handleGoHome = () => {
   router.push('/')
