3fe7bdbf8ce9d8ef6f5af0db760e949e79f07571
Mr.Mikey <v_jpch@digitalgd.com.cn>
2026-03-26 15:52:48 +0800

    chore(deps): 移除 stylelint-config-prettier 并更新依赖版本
    移除了 stylelint-config-prettier 配置包，并对多个依赖包进行了版本更新，
    包括 ts-jest 从 29.x 降级到 27.x，同时更新了 postcss、nanoid、
    picocolors 和 source-map-js 等包的版本。

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,7 +2,6 @@
 module.exports = {
   extends: [
     'stylelint-config-standard', // 配置stylelint拓展css插件
-    'stylelint-config-prettier', // 配置stylelint和prettier兼容
     'stylelint-config-recess-order', // 配置stylelint css属性书写顺序插件
     'stylelint-config-standard-scss', // 配置stylelint scss插件
     'stylelint-config-recommended-vue', // vue项目需要添加
